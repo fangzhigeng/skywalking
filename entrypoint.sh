af01e571096cfe8f760939b64cadb8a563c7a010
fangzhigeng <53166708+fangzhigeng@users.noreply.github.com>
2020-02-07 14:48:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-java -jar app.jar $@
+java \
+    -javaagent:agent/skywalking/skywalking-agent.jar \
+    -Dskywalking.agent.service_name=${SW_SERVICE_NAME:-deepexi-spring-cloud} \
+    -Dskywalking.collector.backend_service=${SW_SERVICE_ADDR} \
+    -jar app.jar $@
